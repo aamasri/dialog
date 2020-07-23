78b163d470087a47d1feffe4d82697bb43563fd4
Ananda Masri <ananda@auroraweb.ca>
2020-07-23 14:08:42 -0600

    Release version 1.1.0 - replaced method exports with a single default

--- publish.js
+++ publish.js
@@ -2,30 +2,37 @@ const packageJson = require('./package.json');
 const version = packageJson.version;
 const description = packageJson.versionDescription;
 
-console.log(`releasing dialog ${version} - ${description}`);
-
 // function to run bash commands
 const execSync = require('child_process').execSync;
 function runShell(command) {
     return execSync(command, { encoding: 'utf-8' });
 }
 
-console.info(`\n  installing npm dependencies...`);
+console.log(`releasing dialog (${version} - ${description})...`);
+
+const lastCommitMessage = runShell('git log -1 --pretty=%B');
+console.log(`  last commit was:`, lastCommitMessage);
+if (lastCommitMessage.includes(version) || lastCommitMessage.includes(description)) {
+    console.error(`  ABORTING: version ${version} - ${description} has already been published!`);
+    process.exitCode = 0;
+}
+
+console.info(`  installing npm dependencies...`);
 runShell('npm install');
 
-console.info(`\n  building browser dist folder...`);
+console.info(`  building browser dist folder...`);
 runShell('npm run build-production');
 
-console.info(`\n  git committing...`);
+console.info(`  git committing...`);
 runShell(`git commit -a -m "Release version ${version} - ${description}"`);
 
-console.info(`\n  git tagging...`);
+console.info(`  git tagging...`);
 runShell(`git tag ${version}`);
 runShell(`git tag -f latest`);
 
-console.info(`\n  git pushing...`);
+console.info(`  git pushing...`);
 runShell(`git push origin master :refs/tags/latest`);
 runShell(`git push origin master --tags`);
 
-console.info(`\n  publishing to npmjs...`);
+console.info(`  publishing to npm...`);
 runShell(`npm publish`);
